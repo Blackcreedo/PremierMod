a516cb823194c466aa0c4468bd9460335d21a225
Blackcreed <elouanderenne@gmail.com>
2021-12-29 13:01:44 +0100

    Titanium and Ruby tools

--- src/main/java/fr/black/pm/item/ModItems.java
+++ src/main/java/fr/black/pm/item/ModItems.java
@@ -5,7 +5,7 @@ import fr.black.pm.item.custom.CoalCokeItem;
 import fr.black.pm.item.custom.Firestone;
 import fr.black.pm.item.custom.SmartBlowTorchItem;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -44,6 +44,43 @@ public class ModItems
 	
 	public static final RegistryObject<Item> COAL_COKE = 
 			ITEMS.register("coal_coke", () -> new CoalCokeItem(new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+
+
+	/* TITANIUM TOOLS */
+	public static final RegistryObject<Item> TITANIUM_SWORD =
+			ITEMS.register("titanium_sword", () -> new SwordItem(ModTiers.TITANIUM, 5,5,
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> TITANIUM_PICKAXE =
+			ITEMS.register("titanium_pickaxe", () -> new PickaxeItem(ModTiers.TITANIUM, 5,5,
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> TITANIUM_AXE =
+			ITEMS.register("titanium_axe", () -> new AxeItem(ModTiers.TITANIUM, 5,5,
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> TITANIUM_SHOVEL =
+			ITEMS.register("titanium_shovel", () -> new ShovelItem(ModTiers.TITANIUM, 5,5,
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> TITANIUM_HOE =
+			ITEMS.register("titanium_hoe", () -> new HoeItem(ModTiers.TITANIUM, 5,5,
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+
+
+	/* RUBY TOOLS */
+	public static final RegistryObject<Item> RUBY_SWORD =
+			ITEMS.register("ruby_sword", () -> new SwordItem(ModTiers.RUBY, 5,5,
+					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> RUBY_PICKAXE =
+			ITEMS.register("ruby_pickaxe", () -> new PickaxeItem(ModTiers.RUBY, 5,5,
+					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> RUBY_AXE =
+			ITEMS.register("ruby_axe", () -> new AxeItem(ModTiers.RUBY, 5,5,
+					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> RUBY_SHOVEL =
+			ITEMS.register("ruby_shovel", () -> new ShovelItem(ModTiers.RUBY, 5,5,
+					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+	public static final RegistryObject<Item> RUBY_HOE =
+			ITEMS.register("ruby_hoe", () -> new HoeItem(ModTiers.RUBY, 5,5,
+					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+
 	
 	
 	
