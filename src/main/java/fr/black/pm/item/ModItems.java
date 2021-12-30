889b48c26f4910fa1d5beddff3b4c0aec190aab0
Blackcreed <elouanderenne@gmail.com>
2021-12-30 11:52:42 +0100

    Titanium and Ruby Armor

--- src/main/java/fr/black/pm/item/ModItems.java
+++ src/main/java/fr/black/pm/item/ModItems.java
@@ -81,31 +81,31 @@ public class ModItems
 	/* RUBY TOOLS AND ARMOR*/
 	public static final RegistryObject<Item> RUBY_SWORD =
 			ITEMS.register("ruby_sword", () -> new SwordItem(ModTiers.RUBY, 5,1f,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_PICKAXE =
 			ITEMS.register("ruby_pickaxe", () -> new PickaxeItem(ModTiers.RUBY, 1,1f,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_AXE =
 			ITEMS.register("ruby_axe", () -> new AxeItem(ModTiers.RUBY, 6,1f,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_SHOVEL =
 			ITEMS.register("ruby_shovel", () -> new ShovelItem(ModTiers.RUBY, 1,1f,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_HOE =
 			ITEMS.register("ruby_hoe", () -> new HoeItem(ModTiers.RUBY, 1,1f,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_HELMET =
 			ITEMS.register("ruby_helmet", () -> new ModArmorItems(ModArmorMaterials.RUBY, EquipmentSlot.HEAD,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_CHESTPLATE =
 			ITEMS.register("ruby_chestplate", () -> new ArmorItem(ModArmorMaterials.RUBY, EquipmentSlot.CHEST,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_LEGGINGS =
 			ITEMS.register("ruby_leggings", () -> new ArmorItem(ModArmorMaterials.RUBY, EquipmentSlot.LEGS,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 	public static final RegistryObject<Item> RUBY_BOOTS =
 			ITEMS.register("ruby_boots", () -> new ArmorItem(ModArmorMaterials.RUBY, EquipmentSlot.FEET,
-					new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
+			new Item.Properties().tab(ModCreativeModeTab.PREMIER_MOD_TAB)));
 
 	
 	
