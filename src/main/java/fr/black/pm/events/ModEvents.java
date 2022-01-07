9f7710a05fae2eda5e72186aacc0a8fefcd91a4b
Elouan <elouanderenne@gmail.com>
2022-01-07 15:30:10 +0100

    ModEvent Set mob on fire when hit with nether brick

---
+++ src/main/java/fr/black/pm/events/ModEvents.java
@@ -0,0 +1,30 @@
+package fr.black.pm.events;
+
+
+
+import fr.black.pm.PremierMod;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = PremierMod.MOD_ID)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void setEntityOnFireWhenHitWithNetherBrick(LivingDamageEvent event) {
+        if(!event.getEntity().level.isClientSide()){
+            if(event.getSource().getDirectEntity() instanceof Player){
+                Player player = (Player) event.getSource().getDirectEntity();
+                if(player.getMainHandItem().getItem() == Items.NETHER_BRICK){
+                    if(!player.isCreative()) {
+                        player.getMainHandItem().shrink(1);
+                    }
+                    event.getEntityLiving().setSecondsOnFire(5);
+                }
+            }
+        }
+    }
+
+}
